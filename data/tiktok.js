12b4b209419925820973ce64b6408ac34ac47854
fikripapay <fikriabdul679@gmail.com>
2026-06-22 09:01:49 +0700

    update

---
+++ data/tiktok.js
@@ -0,0 +1,199 @@
+window.priceData_tiktok = {
+  "tt-followersid": {
+    min: 10,
+    data: [
+      {
+        qty: "100 Followers",
+        warranty: "No Garansi",
+        price: "Rp 10.500",
+        status: "ready",
+      },
+      {
+        qty: "100 Followers",
+        warranty: "30 Hari ♻️",
+        price: "Rp 12.500",
+        status: "ready",
+      },
+      {
+        qty: "250 Followers",
+        warranty: "No Garansi",
+        price: "Rp 22.000",
+        status: "ready",
+      },
+      {
+        qty: "250 Followers",
+        warranty: "30 Hari ♻️",
+        price: "Rp 26.000",
+        status: "ready",
+      },
+      {
+        qty: "500 Followers",
+        warranty: "No Garansi",
+        price: "Rp 39.000",
+        status: "ready",
+      },
+      {
+        qty: "500 Followers",
+        warranty: "30 Hari ♻️",
+        price: "Rp 45.000",
+        status: "ready",
+      },
+      {
+        qty: "750 Followers",
+        warranty: "No Garansi",
+        price: "Rp 56.000",
+        status: "ready",
+      },
+      {
+        qty: "750 Followers",
+        warranty: "30 Hari ♻️",
+        price: "Rp 64.000",
+        status: "ready",
+      },
+      {
+        qty: "1000 Followers",
+        warranty: "No Garansi",
+        price: "Rp 73.000",
+        status: "ready",
+      },
+      {
+        qty: "1000 Followers",
+        warranty: "30 Hari ♻️",
+        price: "Rp 80.000",
+        status: "ready",
+      },
+    ],
+  },
+
+  "tt-views": {
+    min: 10,
+    data: [
+      {
+        qty: "1000 Views",
+        warranty: "No Garansi",
+        price: "Rp 2.000",
+        status: "ready",
+      },
+      {
+        qty: "1000 Views",
+        warranty: "30 Hari ♻️",
+        price: "Rp 3.000",
+        status: "ready",
+      },
+      {
+        qty: "2500 Views",
+        warranty: "No Garansi",
+        price: "Rp 4.000",
+        status: "ready",
+      },
+      {
+        qty: "2500 Views",
+        warranty: "30 Hari ♻️",
+        price: "Rp 5.500",
+        status: "ready",
+      },
+      {
+        qty: "5000 Views",
+        warranty: "No Garansi",
+        price: "Rp 7.500",
+        status: "ready",
+      },
+      {
+        qty: "5000 Views",
+        warranty: "30 Hari ♻️",
+        price: "Rp 10.000",
+        status: "ready",
+      },
+      {
+        qty: "7500 Views",
+        warranty: "No Garansi",
+        price: "Rp 11.000",
+        status: "ready",
+      },
+      {
+        qty: "7500 Views",
+        warranty: "30 Hari ♻️",
+        price: "Rp 14.000",
+        status: "ready",
+      },
+      {
+        qty: "10000 Views",
+        warranty: "No Garansi",
+        price: "Rp 15.000",
+        status: "ready",
+      },
+      {
+        qty: "10000 Views",
+        warranty: "30 Hari ♻️",
+        price: "Rp 19.000",
+        status: "ready",
+      },
+    ],
+  },
+
+  "tt-likes": {
+    min: 10,
+    data: [
+      {
+        qty: "100 Likes",
+        warranty: "No Garansi",
+        price: "Rp 6.000",
+        status: "ready",
+      },
+      {
+        qty: "100 Likes",
+        warranty: "30 Hari ♻️",
+        price: "Rp 8.000",
+        status: "ready",
+      },
+      {
+        qty: "250 Likes",
+        warranty: "No Garansi",
+        price: "Rp 12.000",
+        status: "ready",
+      },
+      {
+        qty: "250 Likes",
+        warranty: "30 Hari ♻️",
+        price: "Rp 15.000",
+        status: "ready",
+      },
+      {
+        qty: "500 Likes",
+        warranty: "No Garansi",
+        price: "Rp 21.000",
+        status: "ready",
+      },
+      {
+        qty: "500 Likes",
+        warranty: "30 Hari ♻️",
+        price: "Rp 25.000",
+        status: "ready",
+      },
+      {
+        qty: "750 Likes",
+        warranty: "No Garansi",
+        price: "Rp 30.000",
+        status: "ready",
+      },
+      {
+        qty: "750 Likes",
+        warranty: "30 Hari ♻️",
+        price: "Rp 35.000",
+        status: "ready",
+      },
+      {
+        qty: "1000 Likes",
+        warranty: "No Garansi",
+        price: "Rp 39.000",
+        status: "ready",
+      },
+      {
+        qty: "1000 Likes",
+        warranty: "30 Hari ♻️",
+        price: "Rp 45.000",
+        status: "ready",
+      },
+    ],
+  },
+};
