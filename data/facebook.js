ebf769ad5e1e77201b51dd4ad361797eeccbfb13
fikripapay <fikriabdul679@gmail.com>
2026-07-21 11:39:51 +0700

    update

--- data/facebook.js
+++ data/facebook.js
@@ -1,113 +1,149 @@
-window.priceData_tiktok = {
-  "tt-followersid": {
+window.priceData_facebook = {
+  "fb-followers": {
     min: 10,
     data: [
       {
         qty: "100 Followers",
         warranty: "Permanen",
-        price: "Rp 9.800",
+        price: "Rp 3.100",
         status: "ready",
       },
       {
         qty: "250 Followers",
         warranty: "Permanen",
-        price: "Rp 24.500",
+        price: "Rp 7.700",
         status: "ready",
       },
       {
         qty: "500 Followers",
         warranty: "Permanen",
-        price: "Rp 48.800",
+        price: "Rp 15.200",
         status: "ready",
       },
       {
         qty: "750 Followers",
         warranty: "Permanen",
-        price: "Rp 73.200",
+        price: "Rp 22.800",
         status: "ready",
       },
       {
         qty: "1000 Followers",
         warranty: "Permanen",
-        price: "Rp 97.500",
+        price: "Rp 30.300",
         status: "ready",
       },
     ],
   },
 
-  "tt-views": {
+  "fb-group": {
+    min: 10,
+    data: [
+      {
+        qty: "100 Members",
+        warranty: "Permanen",
+        price: "Rp 3.100",
+        status: "ready",
+      },
+      {
+        qty: "250 Members",
+        warranty: "Permanen",
+        price: "Rp 7.600",
+        status: "ready",
+      },
+      {
+        qty: "500 Members",
+        warranty: "Permanen",
+        price: "Rp 15.100",
+        status: "ready",
+      },
+      {
+        qty: "750 Members",
+        warranty: "Permanen",
+        price: "Rp 22.600",
+        status: "ready",
+      },
+      {
+        qty: "1000 Members",
+        warranty: "Permanen",
+        price: "Rp 30.100",
+        status: "ready",
+      },
+    ],
+  },
+
+  "fb-views": {
     min: 10,
     data: [
       {
         qty: "1000 Views",
         warranty: "Permanen",
-        price: "Rp 2.200",
+        price: "Rp 2.000",
         status: "ready",
       },
       {
         qty: "2500 Views",
         warranty: "Permanen",
-        price: "Rp 5.300",
+        price: "Rp 3.000",
         status: "ready",
       },
       {
         qty: "5000 Views",
         warranty: "Permanen",
-        price: "Rp 10.500",
+        price: "Rp 4.500",
         status: "ready",
       },
       {
         qty: "7500 Views",
         warranty: "Permanen",
-        price: "Rp 15.800",
+        price: "Rp 5.500",
         status: "ready",
       },
       {
         qty: "10000 Views",
         warranty: "Permanen",
-        price: "Rp 21.000",
+        price: "Rp 7.000",
         status: "ready",
       },
     ],
   },
 
-  "tt-likes": {
+  "fb-likes": {
     min: 10,
     data: [
       {
         qty: "100 Likes",
         warranty: "Permanen",
-        price: "Rp 5.200",
+        price: "Rp 1.500",
         status: "ready",
       },
       {
         qty: "250 Likes",
         warranty: "Permanen",
-        price: "Rp 12.900",
+        price: "Rp 3.700",
         status: "ready",
       },
       {
         qty: "500 Likes",
         warranty: "Permanen",
-        price: "Rp 25.700",
+        price: "Rp 7.400",
         status: "ready",
       },
       {
         qty: "750 Likes",
         warranty: "Permanen",
-        price: "Rp 38.500",
+        price: "Rp 11.100",
         status: "ready",
       },
       {
         qty: "1000 Likes",
         warranty: "Permanen",
-        price: "Rp 51.200",
+        price: "Rp 14.700",
         status: "ready",
       },
     ],
   },
 
-  "tt-comments": {
+  "fb-comments": {
     min: 10,
     data: [
       {
